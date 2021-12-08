b4d2eefd3d7ee2cbdd3502f8008371cf30a79778
dieison <dieison.vidal10@gmail.com>
2021-12-08 12:40:51 -0300

    ajuste na estrutura do projeto

--- src/controllers/persons/controllerPersons.js
+++ src/controllers/persons/controllerPersons.js
@@ -9,28 +9,33 @@ export const persons = [];
 export const controllerPersons = {
 
     async createPerson(request, response) {
-        try {
-            const { cpf, full_name, email, phone, personname, password } = request.body;
-            
-            /* const personAlreadyExists = await Person.findBy("cpf", cpf)
-            console.log(personAlreadyExists)
-            if (!personAlreadyExists) {
-                response.status(400).json({ error: "Person already exists!" });
-            } else{ 
 
-            } */
+        try {
+            const { cpf, full_name, email, phone, username, password } = request.body;
+
             const dataPerson = new Person({
                 cpf,
                 full_name,
                 email,
                 phone,
-                personname,
+                username,
                 password
             });
 
-            const  query = await Person.find({}, {cpf: 1});
-            console.log(query[{cpf}])
-            console.log(dataPerson)
+            //const query = await Person.find({}, { cpf: 1 });
+            const query = await Person.find({});
+            const dataPersonCPF = await query.find(person => person.cpf === cpf)
+
+            console.log(dataPersonCPF)
+                //console.log(dataPerson)
+
+            /* if (!dataPersonCPF) {
+                const person = await dataPerson.save();
+                return response.status(200).json({ succes: "Created successfully!", person });
+            } else {
+                return res.status(400).json({ message: "Person alredy exists" });
+            } */
+
 
             /* if(dataPerson == query){
                 return response.json({message: "Person already exists!"})
@@ -38,8 +43,8 @@ export const controllerPersons = {
             else{
                 const person = await dataPerson.save();
                 return response.status(200).json({ succes: "Created successfully!", person });
-            }  */        
-            
+            }  */
+
 
 
             /* const personSchema = new Schema({
@@ -57,79 +62,49 @@ export const controllerPersons = {
               });
 
               /* console.log(personSchema) */
-              /* if(personSchema){
-                const person = await dataPerson.save();
-                return response.status(200).json({ succes: "Created successfully!", person });
-              } 
-              else {
-                return response.json({message: "Person already exists!"})
-              } */
-
-              
-
-
-            /* const PersonSchema = new Schema({
-                cpf: {type:String}
-            });
-
-            const PersonModel = mongoose.model('PersonModel',PersonSchema);
-            const achou = PersonModel.find({cpf : cpf})
-                console.log(achou.length) */
-            /* function updateperson(person,cb){
-                PersonModel.find({cpf : person.cpf}, function (err, docs) {
-                    console.log(person.cpf)
-                    
-                    if (docs.length){
-                        cb('per exists already',null);
-                    }else{
-                        person.save(function(err){
-                            cb(err,person);
-                        });
-                    }
-                });
+            /* if(personSchema){
+              const person = await dataPerson.save();
+              return response.status(200).json({ succes: "Created successfully!", person });
+            } 
+            else {
+              return response.json({message: "Person already exists!"})
             } */
 
 
-            /* if() {
+            /* const person = await dataPerson.save();
 
-            } else {
-
-            }
-
-            const person = await dataPerson.save();
-                
             if (person) {
                 return response.status(200).json({ succes: "Created successfully!", person });
             } */
-            } catch (error) {
-                return response.status(400).json({ error });
-            }
-        
+        } catch (error) {
+            return response.status(400).json({ error });
+        }
+
     },
 
-    async updatePersonByID(request, response){
-        try{
+    async updatePersonByID(request, response) {
+        try {
             const { id } = request.query;
-        
+
             const dataPerson = request.body;
-           
+
             const updatedPerson = await Person.findByIdAndUpdate(id, dataPerson);
             //console.log(updatedPerson);
             response.json(updatedPerson);
-        } catch(err) {
-            response.json({error: "Person not found"});
+        } catch (err) {
+            response.json({ error: "Person not found" });
         }
     },
 
-    async deletePersonByID(request, response){
-        try{
+    async deletePersonByID(request, response) {
+        try {
             const { id } = request.query;
-           
+
             const deletedPerson = await Person.findByIdAndDelete(id);
-            
-            response.json({message: "Person deleted", deletedPerson});
+
+            response.json({ message: "Person deleted", deletedPerson });
         } catch {
-            response.json({error: "Person not found"});
+            response.json({ error: "Person not found" });
         }
     },
 
@@ -138,23 +113,23 @@ export const controllerPersons = {
     },
 
     async listPersons(request, response) {
-        try{
+        try {
             const people = await Person.find({});
-            response.json({people});
+            response.json({ people });
         } catch (err) {
-            response.json({error: "People not found!"});
+            response.json({ error: "People not found!" });
         }
     },
 
     async showPersonByID(request, response) {
-        try{
+        try {
             const { id } = request.query;
-           
+
             const person = await Person.findById(id);
-            
+
             response.json(person);
         } catch (err) {
-            response.json({error: "Person not found"})
+            response.json({ error: "Person not found" })
         }
     }
 }
