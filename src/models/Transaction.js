f7e3bf439732411c59db1beedcf152ae5b171479
dieison vidal <dieison.vidal10@gmail.com>
2021-12-27 16:52:46 -0300

    Arquivos atualizados

---
+++ src/models/Transaction.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const TransactionModel = {
+    number_account: {
+
+    }
+    
+}
+
+const Transaction = mongoose.model('Transaction', TransactionModel);
+
+export default Transaction;
